fff4010fd9fe53efda02468949210f9cc73dd8d1
cartheur <cartheur@pm.me>
2025-02-21 15:43:34 +0100

    Implement record feature

--- Composition/App/Program.cs
+++ Composition/App/Program.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Threading.Tasks;
+using Cartheur.Presents;
 
 namespace App
 {
     class Program
     {
-        static void Main(string[] args)
+        public static Recorder VoiceRecorder { get; private set; }
+
+        static async Task Main(string[] args)
         {
             // Create an instance of the Classifier
             var classifier = new EmotionClassification.Classifier();
@@ -12,6 +16,13 @@ namespace App
             // Load the dataset
             string datasetDirectoryPath = "/home/cartheur/ame/aiventure/aiventure-github/voice/voice-emotion-detector/datasets";
             classifier.LoadData(datasetDirectoryPath);
+
+            VoiceRecorder = new Recorder();
+                await VoiceRecorder.Record(ReturnRecordingFilePath("recorded"), 500);
+                Console.WriteLine("Started recording...");
+                VoiceRecorder.RecordingFinished += RecordingEvent;
+                // Send over for the file for analysis and return the approximated emotion.
+                //DetectEmotion = new Boagaphish.Analytics.DetectEmotion(ReturnRecordingFilePath(OutputDetectionFileName), "linux");
 
             Console.WriteLine("Enter the path to the audio file for emotion classification:");
             string audioFilePath = Console.ReadLine();
@@ -21,6 +32,16 @@ namespace App
 
             // Display the classification result
             Console.WriteLine($"The predicted emotion is: {emotion}");
+
+            static string ReturnRecordingFilePath(string filename)
+            {
+                return $"/home/cartheur/ame/aiventure/aiventure-github/voice/voice-emotion-detector/Composition/App/recordings/{filename}.wav";
+            }
+        }
+
+        private static void RecordingEvent(object source, EventArgs e)
+        {
+            Console.WriteLine("Recording has completed.");
         }
     }
 }
