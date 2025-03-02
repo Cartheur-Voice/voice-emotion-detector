31002831fe54de56901d9371bd16ab17a3168342
cartheur <cartheur@pm.me>
2025-03-02 14:33:06 +0100

    Move along

--- Composition/App/Program.cs
+++ Composition/App/Program.cs
@@ -3,8 +3,9 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Cartheur.Presents;
-using Cartheur.Animals.Utilities;
+//using Cartheur.Animals.Utilities;
 using Cartheur.Animals.Core;
+using Boagaphish;
 
 namespace App
 {
@@ -12,7 +13,7 @@ namespace App
     {
         public static Recorder VoiceRecorder { get; private set; }
         public static int RecordingDuration { get; private set; }
-        public static LoaderPaths Configuration;
+        public static Cartheur.Presents.LoaderPaths Configuration;
         private static Aeon _thisAeon;
         public static string TrainingDataFiles { get; private set; }
         public static string FileName { get; private set; }
@@ -21,31 +22,33 @@ namespace App
         static async Task Main(string[] args)
 {
             // Create the app with settings.
-            Configuration = new LoaderPaths("Debug");
-            _thisAeon = new Aeon("dot");
+            Configuration = new Cartheur.Presents.LoaderPaths("Debug");
+            //_thisAeon = new Aeon("dot");
             // Load the given application configuration.
-            _thisAeon.LoadSettings(Configuration.PathToSettings);
-            TrainingDataFiles = _thisAeon.GlobalSettings.GrabSetting("trainingdatafiles");
-            FileName = _thisAeon.GlobalSettings.GrabSetting("filename");
-            UseFile = Convert.ToBoolean(_thisAeon.GlobalSettings.GrabSetting("usefile"));
+            //_thisAeon.LoadSettings(Configuration.PathToSettings);
+            //TrainingDataFiles = _thisAeon.GlobalSettings.GrabSetting("trainingdatafiles");
+            //FileName = _thisAeon.GlobalSettings.GrabSetting("filename");
+            //UseFile = Convert.ToBoolean(_thisAeon.GlobalSettings.GrabSetting("usefile"));
             // Create an instance of the Classifier
             var classifier = new EmotionClassification.Classifier();
-            classifier.LoadData(TrainingDataFiles);
+            classifier.LoadData(Configuration.PathToTrainingData);
+            var path = Path.Combine(LoaderPaths.ActiveRuntime, LoaderPaths.SavePath, FileName + ".wav");
+            Console.WriteLine(Terminal.StartBashProcess("arecord -vv -r 16000 -c 1 -f S16_LE -d 1 " + path));
             // Set the recording duration
-            RecordingDuration = 1000;
-            if (!UseFile)
-            {
-                VoiceRecorder = new Recorder();
-                await VoiceRecorder.Record(ReturnRecordingFilePath("recorded"), RecordingDuration);
-                Console.WriteLine("Started recording...");
-                VoiceRecorder.RecordingFinished += RecordingEvent;
-            }
+            //RecordingDuration = 1000;
+            // if (!UseFile)
+            // {
+            //     VoiceRecorder = new Recorder();
+            //     await VoiceRecorder.Record(ReturnRecordingFilePath("recorded"), RecordingDuration);
+            //     Console.WriteLine("Started recording...");
+            //     VoiceRecorder.RecordingFinished += RecordingEvent;
+            // }
             Console.WriteLine("Getting the file for analysis...");
-            //string audioFilePath = ReturnRecordingFilePath("recorded");
+            string audioFilePath = ReturnRecordingFilePath("recorded");
             // Send over for the file for analysis and return the approximated emotion.
             try
             {
-                //DetectEmotion = new Boagaphish.Analytics.DetectEmotion(ReturnRecordingFilePath(OutputDetectionFileName), "linux");
+                DetectEmotion = new Boagaphish.Analytics.DetectEmotion(ReturnRecordingFilePath(OutputDetectionFileName), "linux");
             }
             catch (Exception ex)
             {
@@ -65,7 +68,7 @@ namespace App
 
             static string ReturnRecordingFilePath(string filename)
             {
-                return $"/home/cartheur/ame/aiventure/aiventure-github/voice/voice-emotion-detector/Composition/App/recordings/{filename}.wav";
+                return Path.Combine(LoaderPaths.ActiveRuntime, LoaderPaths.SavePath, filename + ".wav");
             }
         }
 
